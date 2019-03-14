63ef7e6afcf75da403a9175812b36ca6ad0b197c
Caleb Young <calebyoung04@gmail.com>
2019-03-14 14:28:58 -0600

    Switch to using npm mysql2. Get manifest working.

--- manifest.js
+++ manifest.js
@@ -1,6 +1,6 @@
 var request = require("request");
 var auth = require("./auth.json");
-var mysql = require("mysql");
+var mysql = require("mysql2");
 
 const ROOT_PATH = "https://www.bungie.net/Platform";
 const BASE_URL = "https://www.bungie.net"
@@ -15,103 +15,138 @@ var requestHeader =
 };
 
 var connection = mysql.createConnection({
-    host: auth.Host,
-    user: auth.User,
-    password: auth.Password,
-    database: auth.Database
+    host: auth.host,
+    user: auth.user,
+    password: auth.password,
+    database: auth.database,
+    port: auth.port
 });
 
-connection.connect(function(err) {
-    if (err) { console.log(err.toString()); }
-    else { console.log("Connected!"); }
-});
-
-connection.query("SELECT * FROM path", function(error, results, fields) {
-    if (error) console.log(error.toString());
-    else (console.log(results[0].latest));
-});
-
-connection.end();
-
-
-
-// var FindManifestPath = function () {
-//     return new Promise((resolve, reject) => {
-//         requestHeader["url"] = ROOT_PATH + "/Destiny2/Manifest/";
-
-//         request(requestHeader, function (error, response, body) {
-//             if (!error && response.statusCode == 200) {
-//                 let results = JSON.parse(body);
-
-//                 if (results["Response"] === undefined) {
-//                     reject("FindManifest rejected the Promise");
-//                 }
-//                 else {
-//                     //resolve(results);
-//                     //resolve(results["Response"]["mobileWorldContentPaths"]["en"]);
-//                     resolve(results["Response"]["jsonWorldContentPaths"]["en"]);
-//                 }
-//             }
-//         });
-//     });
-// };
-
-// var FindManifest = function (path) {
-//     return new Promise((resolve, reject) => {
-//         requestHeader["url"] = BASE_URL + path;
-
-//         request(requestHeader, function (error, response, body) {
-//             if (!error && response.statusCode == 200) {
-//                 //resolve(body);
-//                 let results = JSON.parse(body);
-
-//                 resolve(results);
-//             }
-//         });
-//     });
-// };
-
-// var RebuildManifestDB = function (jsonManifest) {
-//     return new Promise((resolve, reject) => {
-//         connection.connect(function (err) {
-//             if (err) { reject("Connection to database failed"); }
-//         });
-
-//         for (let definition in jsonManifest) {
-//             let table = definition.toString();
-//             connection.query("create table if not exists " + table + " (hash BIGINT, value JSON)");
-
-//             connection.query("delete from " + table);
-
-//             for (let hash in jsonManifest[table]) {
-//                 connection.query("insert into " + table + " (hash, value) values ("
-//                     + hash + ", \'" + JSON.stringify(jsonManifest[table][hash]) + "\')");
-//             }
-
-//             // console.log("\"" + table + "\"");
-//         }
-//         resolve("Database built");
-//     });
-// };
-
-// var GetManifest = async function () {
-//     let path = await FindManifestPath();
-//     let manifest = await FindManifest(path);
-//     let built = await RebuildManifestDB(manifest);
-
-//     return built;
-// };
-
-
-// GetManifest().then(results => {
-//     console.log(results);
-// }).catch(message => {
-//     console.log(message.toString());
-// }).finally(() => {
-//     connection.end();
+// connection.connect(function(err) {
+//     if (err) { console.log(err.toString()); }
+//     else { console.log("Connected!"); }
 // });
 
+// connection.query("SELECT * FROM path", function(error, results, fields) {
+//     if (error) console.log(error.toString());
+//     else (console.log(results[0].latest));
+// });
 
+// connection.end();
+
+
+
+var FindManifestPath = function () {
+    return new Promise((resolve, reject) => {
+        requestHeader["url"] = ROOT_PATH + "/Destiny2/Manifest/";
+
+        request(requestHeader, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                let results = JSON.parse(body);
+
+                if (results["Response"] === undefined) {
+                    reject("FindManifest rejected the Promise");
+                }
+                else {
+                    //resolve(results);
+                    //resolve(results["Response"]["mobileWorldContentPaths"]["en"]);
+                    resolve(results["Response"]["jsonWorldContentPaths"]["en"]);
+                }
+            }
+        });
+    });
+};
+
+var FindManifest = function (path) {
+    return new Promise((resolve, reject) => {
+        requestHeader["url"] = BASE_URL + path;
+
+        request(requestHeader, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //resolve(body);
+                let results = JSON.parse(body);
+
+                resolve(results);
+            }
+        });
+    });
+};
+
+var RebuildManifestDB = function (jsonManifest) {
+    return new Promise((resolve, reject) => {
+        connection.connect(function (err) {
+            if (err) { reject("Connection to database failed"); }
+        });
+
+        for (let definition in jsonManifest) {
+            let table = definition.toString();
+            let createTable = "create table if not exists " + table + " (hash BIGINT NOT NULL UNIQUE, value JSON, PRIMARY KEY(hash))";
+            connection.query(createTable);
+
+            let deleteTableData = "delete from " + table;
+            connection.query(deleteTableData);
+
+            for (let hash in jsonManifest[table]) {
+                let jsonValue = JSON.stringify(jsonManifest[table][hash]);
+                let replacedJson = mysql_real_escape_string(jsonValue);
+
+                let addTableData = "insert into " + table + "(hash, value) values (" 
+                                    + hash + ", \'" + replacedJson + "\')"; 
+                connection.query(addTableData, function (error) {
+                    if (error) { console.log(addTableData); }
+                });
+            }
+
+            // console.log(jsonManifest["DestinySandboxPerkDefinition"]);
+
+            // console.log("\"" + table + "\"");
+            // console.log(table);
+        }
+        resolve("Database built");
+    });
+};
+
+var GetManifest = async function () {
+    let path = await FindManifestPath();
+    let manifest = await FindManifest(path);
+    let built = await RebuildManifestDB(manifest);
+
+    return built;
+};
+
+
+GetManifest().then(results => {
+    console.log(results);
+}).catch(message => {
+    console.log(message.toString());
+}).finally(() => {
+    connection.end();
+});
+
+function mysql_real_escape_string(str) {
+    return str.replace(/[\0\x08\x09\x1a\n\r'\\\%]/g, function (char) {
+        switch (char) {
+            case "\0":
+                return "\\0";
+            case "\x08":
+                return "\\b";
+            case "\x09":
+                return "\\t";
+            case "\x1a":
+                return "\\z";
+            case "\n":
+                return "\\n";
+            case "\r":
+                return "\\r";
+            case "\"":
+            case "'":
+            case "\\":
+            case "%":
+                return "\\" + char; // prepends a backslash to backslash, percent,
+            // and double/single quotes
+        }
+    });
+}
 
 
 
